e5f4b3b13c7eee3db23b1532c67bc05bbac05249
PatitoCorrea <rafaelcorrealopez21@gmail.com>
2024-10-30 15:31:19 -0500

    resolucion del ejercicio 5

--- ProyectoTaller4/src/Ejercicio5.java
+++ ProyectoTaller4/src/Ejercicio5.java
@@ -1,12 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author patito
- */
+import java.util.Scanner;
 public class Ejercicio5 {
-    
+    public static void main(String[] args) {
+        double cpu, keyboard, pantalla, raton, costo_total;
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("INGRESE EL COSTO DEL CPU:");
+        cpu = teclado.nextDouble();
+        System.out.print("INGRESE EL COSTO DEL KEYBOARD:");
+        keyboard = teclado.nextDouble();
+        System.out.print("INGRESE EL COSTO DE LA PANTALLA:");
+        pantalla = teclado.nextDouble();
+        System.out.print("INGRESE EL COSTO DEL RATON:");
+        raton = teclado.nextDouble();
+        costo_total = cpu + keyboard + pantalla + raton;
+        System.out.println("EL COSTO TOTAL DE LA COMPUTADORA ES: " + costo_total);
+    }
 }
--- ProyectoTaller4/src/Ejercicio6.java
+++ ProyectoTaller4/src/Ejercicio6.java
@@ -1,12 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author patito
- */
+import java.util.Scanner;
 public class Ejercicio6 {
-    
+    public static void main(String[] args) {
+        double monto_prestamo, interes_mensual, plazo_meses, pago_mensualp;
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("INGRESE EL VALOR DEL MONTO DEL PRESTAMO: ");
+        monto_prestamo = teclado.nextDouble();
+        System.out.print("INGRESE EL VALOR DEL INTERES MENSUAL EN %: ");
+        interes_mensual = teclado.nextDouble() / 100;
+        System.out.println("INGRESE EL PLAZO DEL PRESTAMO EN MESES: ");
+        plazo_meses = teclado.nextDouble();
+        pago_mensualp = (monto_prestamo * interes_mensual) / 1 - Math.pow(1 + interes_mensual, -plazo_meses);
+        System.out.println("EL PAGO MENSUAL DEL PRESTAMO ES: " + pago_mensualp);
+    }       
 }
